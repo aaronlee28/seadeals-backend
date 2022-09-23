7dbbd02324f428ca623f78ae01ba9b7c4b4c4d48
reivaldo.julianto <reivaldo.julianto@shopee.com>
2022-09-24 03:14:05 +0700

    refactor(search-list-product): change name to be more consistent to other response

--- dto/searched_product_res.go
+++ dto/searched_product_res.go
@@ -2,38 +2,28 @@ package dto
 
 import (
 	"seadeals-backend/model"
-	"time"
 )
 
 type SearchedProductRes struct {
-	ProductID   uint      `json:"product_id" binding:"required"`
-	ProductName string    `json:"product_name"`
-	Slug        string    `json:"slug" binding:"required"`
-	MediaURL    string    `json:"media_url" binding:"required"`
-	MinPrice    uint      `json:"min_price" binding:"required"`
-	MaxPrice    uint      `json:"max_price" binding:"required"`
-	PromoPrice  float64   `json:"promo_price" binding:"required"`
-	Rating      float64   `json:"rating" binding:"required"`
-	Views       int       `json:"views" binding:"required"`
-	Bought      int       `json:"bought" binding:"required"`
-	City        string    `json:"city" binding:"required"`
-	Category    string    `json:"category" binding:"required"`
-	UpdatedAt   time.Time `json:"updated_at" binding:"required"`
+	MinPrice       uint `json:"min_price" binding:"required"`
+	MaxPrice       uint `json:"max_price" binding:"required"`
+	*GetProductRes `json:"product"`
 }
 
 func (_ *SearchedProductRes) FromProduct(t *model.Product) *SearchedProductRes {
 	return &SearchedProductRes{
-		ProductID:   t.ID,
-		ProductName: t.Name,
-		Slug:        t.Slug,
-		MediaURL:    "",
-		MinPrice:    0,
-		MaxPrice:    0,
-		PromoPrice:  0,
-		Rating:      0,
-		Bought:      0,
-		City:        "",
-		Category:    "",
-		UpdatedAt:   t.UpdatedAt,
+		MinPrice: 0,
+		MaxPrice: 0,
+		GetProductRes: &GetProductRes{
+			ID:            t.ID,
+			Price:         0,
+			Name:          t.Name,
+			Slug:          t.Slug,
+			PictureURL:    "",
+			City:          "",
+			Rating:        0,
+			TotalReviewer: 0,
+			TotalSold:     0,
+		},
 	}
 }
