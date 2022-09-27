d6cdc10dad9a9b97d4200247a95450ad764722b4
rafly.nagachi <rafly.nagachi@shopee.com>
2022-09-27 13:52:41 +0700

    fix(general): request validator for non binding error

--- middleware/request_validator.go
+++ middleware/request_validator.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"net/http"
+	"seadeals-backend/apperror"
 	"seadeals-backend/dto"
 )
 
@@ -11,17 +12,21 @@ func RequestValidator(creator func() any) gin.HandlerFunc {
 	return func(context *gin.Context) {
 		model := creator()
 		if err := context.ShouldBindJSON(&model); err != nil {
-			var e []map[string]any
-			for _, valErr := range err.(validator.ValidationErrors) {
-				tmp := map[string]any{"key": valErr.Namespace(), "tag": valErr.Tag()}
-				e = append(e, tmp)
+			if len(err.(validator.ValidationErrors)) != 0 {
+				var e []map[string]any
+				for _, valErr := range err.(validator.ValidationErrors) {
+					tmp := map[string]any{"key": valErr.Namespace(), "tag": valErr.Tag()}
+					e = append(e, tmp)
+				}
+				badRequest := &dto.AppResponse{
+					StatusCode: http.StatusBadRequest,
+					Status:     "BAD_REQUEST_ERROR",
+					Data:       e,
+				}
+				context.AbortWithStatusJSON(badRequest.StatusCode, badRequest)
+				return
 			}
-			badRequest := &dto.AppResponse{
-				StatusCode: http.StatusBadRequest,
-				Status:     "BAD_REQUEST_ERROR",
-				Data:       e,
-			}
-			context.AbortWithStatusJSON(badRequest.StatusCode, badRequest)
+			context.AbortWithStatusJSON(http.StatusBadRequest, apperror.BadRequestError(err.Error()))
 			return
 		}
 
