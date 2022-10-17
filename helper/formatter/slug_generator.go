357d374a3c2ba58d3294ac4a4b1e9557fb98b90f
yohan.wijaya <yohan.wijaya@shopee.com>
2022-10-17 13:07:05 +0700

    Fix(General): Improved Slug Generator

--- helper/formatter/slug_generator.go
+++ helper/formatter/slug_generator.go
@@ -11,7 +11,7 @@ func GenerateSlug(title string) string {
 		return title + "-slug"
 	}
 
-	nonAlphanumericRegex := regexp.MustCompile(`^[a-zA-Z\d\-_\s]+`)
+	nonAlphanumericRegex := regexp.MustCompile(`^[a-zA-Z\d\-_\s(1)]+`)
 	split := strings.Split(title, " ")
 	slug := strings.Join(split, "-")
 	split = nonAlphanumericRegex.FindAllString(slug, -1)
